ead1394d50e423a98876031f4693826f233022e0
danny dev <devinfo797@gmail.com>
2022-06-04 15:47:40 +0300

    added link for pdf file

--- users/urls.py
+++ users/urls.py
@@ -1,13 +1,14 @@
-from django.urls import path 
+from django.urls import path
 from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
-    path('register/',views.register_user, name='register'),
-    
-    path('profile/',views.profile, name='profile'),
-    path('edit/',views.edit_profile, name='edit'),
-    
-    path('login/',auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
-    path('logout/',auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
+    path('register/', views.register_user, name='register'),
+
+    path('profile/', views.profile, name='profile'),
+    path('get_pdf/', views.pdf_report, name='get_pdf'),
+    path('edit/', views.edit_profile, name='edit'),
+
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
 ]
